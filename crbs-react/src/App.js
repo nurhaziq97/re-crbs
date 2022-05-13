5100395adbdbfecec0124a866852f70f2e194f7b
nurhaziq97 <muhammadnurhaziq97@gmail.com>
2022-05-13 16:42:51 +0800

    reorganize directories

--- crbs-react/src/App.js
+++ crbs-react/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavbarHeader from "./components/Navbar.component";
 
 const App = () => {
   
   return(
     <div> 
       <BrowserRouter>
+          <NavbarHeader />
         <Routes>
           {/* set the route path here */}
           <Route path="" element=""/>
---
+++ src/test/java/com/haziq/crbs/CRBSApplicationTest.java
@@ -0,0 +1,13 @@
+package com.haziq.crbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CRBSApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
